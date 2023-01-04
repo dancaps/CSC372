65883306b03c4c605c4f2b5e125b9f8dc631f09d
dancaps <danny.caperton@gmail.com>
2023-01-04 09:51:13 -0700

    UPDATE: Finished the application to meet the project requirements

--- src/com/dancaps/m3/src/main/java/com/dancaps/m3/CTA3Application.java
+++ src/com/dancaps/m3/src/main/java/com/dancaps/m3/CTA3Application.java
@@ -1,3 +1,12 @@
+/*
+ * Assignment Title: Creating a User Interface I
+ * Name: Danny Caperton
+ * School: Colorado State University Global
+ * Course: 22WB-CSC372-1
+ * Instructor: Dr. Brian Holbert
+ * Due Date: January 8, 2023
+ */
+
 package com.dancaps.m3;
 
 import javafx.application.Application;
@@ -8,9 +17,10 @@ import java.io.IOException;
 
 public class CTA3Application extends Application {
 
-
     @Override
     public void start(Stage stage) throws IOException {
+        /* Sets up the application */
+
         FXMLLoader fxmlLoader = new FXMLLoader(CTA3Application.class.getResource("cta3-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 250);
         stage.setTitle("Working with JavaFX Menus");
@@ -19,6 +29,8 @@ public class CTA3Application extends Application {
     }
 
     public static void main(String[] args) {
+        /* Starts the application */
+
         launch();
     }
 }
--- src/com/dancaps/m3/src/main/java/com/dancaps/m3/CTA3Controller.java
+++ src/com/dancaps/m3/src/main/java/com/dancaps/m3/CTA3Controller.java
@@ -1,75 +1,91 @@
 /*
- * Assignment Title: Creating a GUI Bank Balance Application
+ * Assignment Title: Creating a User Interface I
  * Name: Danny Caperton
  * School: Colorado State University Global
  * Course: 22WB-CSC372-1
  * Instructor: Dr. Brian Holbert
- * Due Date: January 1, 2023
+ * Due Date: January 8, 2023
  */
 
 package com.dancaps.m3;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
-
+import javafx.scene.layout.VBox;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Random;
 
 public class CTA3Controller {
+
+    @FXML
+    public VBox backgroundColor;
     @FXML
     private Label labelText;
-//    @FXML
-//    private BackgroundFill bf = new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY);
-//    @FXML
-//    private Background bg = new Background(bf);
-
-    private final String assignmentInformation = "Assignment Title: Creating a User Interface I\n" +
-            "Name: Danny Caperton\n" +
-            "School: Colorado State University Global\n" +
-            "Course: 22WB-CSC372-1\n" +
-            "Instructor: Dr. Brian Holbert\n" +
-            "Due Date: January 8, 2023\n";
+    private final String assignmentInformation = """
+            Assignment Title: Creating a User Interface I
+            Name: Danny Caperton
+            School: Colorado State University Global
+            Course: 22WB-CSC372-1
+            Instructor: Dr. Brian Holbert
+            Due Date: January 8, 2023""";
 
     @FXML
     private void changeBackgroundColor() {
-        //vbox.setStyle(backgroundColor);
-        labelText.setText("Changing the background color");
+        /* Changes the background to a hue of green and keeps the initial color for the entire program execution */
+
+        // Hues of green chart located here: https://www.rapidtables.com/web/color/green-color.html
+        String[] colors = {"lawngreen", "chartreuse", "limegreen", "lime", "forestgreen", "green", "darkgreen",
+                "greenyellow", "yellowgreen", "springgreen", "mediumspringgreen", "lightgreen", "palegreen",
+                "darkseagreen", "seagreen", "olive", "darkolivegreen", "olivedrab"};
+        // Generates a random number that corresponds to an array element
+        int random = new Random().nextInt(18);
+
+        // Check if the color is in use so that the initial color is never changed
+        for (String color: colors) {
+            if (this.backgroundColor.getStyle().equals("-fx-background-color: " + color)) {
+                return;
+            }
+        }
+
+        // If the color is not in the array the background will be set
+        backgroundColor.setStyle("-fx-background-color: " + colors[random]);
     }
 
     @FXML
     private void exitTheApplication() {
+        /* Exits the program */
+
         Platform.exit();
     }
     @FXML
     private void getAssignmentInformation() {
+        /* Additional functionality to display the assignment information to the application */
+
         labelText.setText(assignmentInformation);
     }
 
     @FXML
     private void getDateTime() {
-        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDate.now(),
-                                                       LocalTime.now(),
+        /* Gets the current date and time and displays it to the application */
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDate.now(), LocalTime.now(),
                                                        ZoneId.of("America/Denver"));
         labelText.setText(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL).format(zonedDateTime));
     }
 
     @FXML
     private void writeTextFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("CTA3.txt"))) {
+        /* Writes the displayed application information to a text file with the assignment information as a heading */
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt"))) {
             writer.write(assignmentInformation);
-            writer.newLine();
-            writer.write("*** Below is the display information. ***\n");
-            writer.newLine();
+            writer.write("\n\n*** Below is the display information. ***\n\n");
             writer.write(labelText.getText());
             labelText.setText("The file was created.");
         } catch (IOException e) {
